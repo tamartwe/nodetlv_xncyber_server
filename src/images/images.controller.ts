146a9b6d6391e674318f762b582b7a8d05fdd9c7
tamar.twena <tamar.twena@xmcyber.com>
2020-02-24 23:17:08 +0200

    make paths relative to resources folder

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Res, Query } from '@nestjs/common';
 import { Response } from 'express';
+import * as pathLib from 'path';
 
 @Controller('images')
 export class ImagesController {
   @Get('*')
   get(@Res() res: Response, @Query('filePath') filePath: string): void {
-    res.sendFile(filePath);
+    const path  = pathLib.join(__dirname, '..', '..', 'resources', filePath);
+    res.sendFile(path);
   }
 }
 
