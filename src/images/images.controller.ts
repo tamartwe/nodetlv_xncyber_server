3509b7ed8a91d12a165462f67b6404e5a4698380
tamar.twena <tamar.twena@xmcyber.com>
2020-02-25 00:19:39 +0200

    sanitize files

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Res, Query } from '@nestjs/common';
 import { Response } from 'express';
 import * as pathLib from 'path';
+import { sanitize } from "sanitize-filename-ts";
 
 @Controller('images')
 export class ImagesController {
   @Get('*')
   get(@Res() res: Response, @Query('filePath') filePath: string): void {
-    const path  = pathLib.join(__dirname, '..', '..', 'resources', filePath);
+    const path  = pathLib.join(__dirname, '..', '..', 'resources', sanitize(filePath));
     res.sendFile(path);
   }
 }
